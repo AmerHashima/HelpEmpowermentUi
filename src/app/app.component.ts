9a72a256981becf1d8c57fc95e3a05e65970cfdb
Amer Hashima <ameer.hashima@hispeed-erp.com>
2026-02-01 01:33:22 +0200

    feat: implement initial API service integration in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 // src\app\app.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import ApiService from './shared/Services/ApiService/api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,29 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Help Empowerment';
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.loadHeaders();
+  }
+
+  private loadHeaders(): void {
+    const body = {
+      filters: [],
+      sort: [],
+      pagination: {
+        getAll: true,
+        pageNumber: 1,
+        pageSize: 10
+      },
+      columns: []
+    };
+
+    this.apiService.get<any>('WeatherForecast').subscribe({
+      next: (response) => console.log('headers search response', response),
+      error: (error) => console.error('headers search error', error)
+    });
+  }
 }
