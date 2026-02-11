0a314c018c9335c21951df06d1821b76e75b1255
Amer Hashima <ameer.hashima@hispeed-erp.com>
2026-02-11 13:28:34 +0200

    Enhance ApiService to include success messages for post, put, and delete operations

--- src/app/shared/Services/ApiService/api.service.ts
+++ src/app/shared/Services/ApiService/api.service.ts
@@ -1,14 +1,18 @@
 // src\app\shared\Services\ApiService\api.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { ToastingMessagesService } from '../ToastingMessages/toasting-messages.service';
 
 @Injectable({ providedIn: 'root' })
 export default class ApiService {
   private readonly baseUrl: string = environment.baseUrl;
   // private token='';
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private toasting: ToastingMessagesService
+  ) {
 
   }
 
@@ -41,27 +45,45 @@ export default class ApiService {
     return this.http.get<T>(fullUrl, { headers: this.createHeaders() });
   }
 
-  post<T>(url: string, body: any): Observable<T> {
+  post<T>(url: string, body: any, successMessage: string = 'Success'): Observable<T> {
     return this.http.post<T>(`${this.baseUrl}/${url}`, body, {
       headers: this.createHeaders(),
-    });
+    }).pipe(
+      tap(() => {
+        if (successMessage) {
+          this.toasting.showToast(successMessage, 'success');
+        }
+      })
+    );
   }
 
 
-  put<T>(url: string, id: string, body: any): Observable<T> {
+  put<T>(url: string, id: string, body: any, successMessage: string = 'Success'): Observable<T> {
     // const fullUrl = `${this.baseUrl}/${url}/${id}`;
     const fullUrl = `${this.baseUrl}/${url}`;
-    return this.http.put<T>(fullUrl, body, { headers: this.createHeaders() });
+    return this.http.put<T>(fullUrl, body, { headers: this.createHeaders() }).pipe(
+      tap(() => {
+        if (successMessage) {
+          this.toasting.showToast(successMessage, 'success');
+        }
+      })
+    );
   }
 
   // delete<T>(url: string, params?: Record<string, any>, token?: string): Observable<T> {
-  delete<T>(url: string, id: string): Observable<T> {
+  delete<T>(url: string, id: string, successMessage: string = 'Success'): Observable<T> {
     // const httpParams = new HttpParams({ fromObject: params || {} });
     const fullUrl = `${this.baseUrl}/${url}/${id}`;
     return this.http.delete<T>(fullUrl, {
       // params: httpParams,
       headers: this.createHeaders(),
-    });
+    }).pipe(
+      tap(() => {
+        if (successMessage) {
+          this.toasting.showToast(successMessage, 'success');
+        }
+      })
+    );
   }
 
   query<T>(url: string, body: any): Observable<T> {
