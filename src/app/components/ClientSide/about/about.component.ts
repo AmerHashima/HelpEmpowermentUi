be659fc23830e57dea0d874830792e1bc100b8b7
ShaimaaAKamal <shaimaakamal039@gmail.com>
2026-02-14 00:42:23 +0200

    finish about

--- src/app/components/ClientSide/about/about.component.ts
+++ src/app/components/ClientSide/about/about.component.ts
@@ -1,12 +1,18 @@
 // src\app\components\about\about.component.ts
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PageBannerComponent } from '../../../shared/clientSide/page-banner/page-banner.component';
+import { Shared } from '../../../shared/Services/shared/shared';
+import { FeatureComponent } from '../../../shared/clientSide/feature/feature.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [PageBannerComponent,FeatureComponent,TranslatePipe],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-
+private shared=inject(Shared);
+isRTL=this.shared.isRtl;
+  aboutVideo='assets/videos/about.mp4'
 }
