ba7e251c394ab345dcedb2339a5facc46307741d
ShaimaaAKamal <shaimaakamal039@gmail.com>
2026-02-13 06:37:00 +0200

    initiate webinar componnent

--- src/app/components/ClientSide/certifications/webinar/webinar.component.ts
+++ src/app/components/ClientSide/certifications/webinar/webinar.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Shared } from '../../../../shared/Services/shared/shared';
+import { PageBannerComponent } from '../../../../shared/clientSide/page-banner/page-banner.component';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-webinar',
-  imports: [],
+  imports: [PageBannerComponent,TranslatePipe],
   templateUrl: './webinar.component.html',
   styleUrl: './webinar.component.scss'
 })
 export class WebinarComponent {
+  private shared = inject(Shared);
+  isRTL = this.shared.isRtl;
 
+  courseImage = "assets/images/webinar/webinar.jpeg";
+
+  registerNow() { }
 }
