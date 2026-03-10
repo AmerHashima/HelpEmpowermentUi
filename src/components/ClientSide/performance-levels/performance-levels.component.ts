e7f5f1973ca09329d7294311804e96d064ddb9c4
Amer Hashima <ameer.hashima@hispeed-erp.com>
2026-03-10 19:36:28 +0200

    add NgFor import to certification-contents component and update imports in performance-levels component

--- src/components/ClientSide/performance-levels/performance-levels.component.ts
+++ src/components/ClientSide/performance-levels/performance-levels.component.ts
@@ -1,8 +1,11 @@
+// src\components\ClientSide\performance-levels\performance-levels.component.ts
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { PerformanceIndicatorsTableComponent } from '../performance-indicators-table/performance-indicators-table.component';
 
 @Component({
   selector: 'app-performance-levels',
-  imports: [],
+  imports: [TranslateModule, PerformanceIndicatorsTableComponent],
   templateUrl: './performance-levels.component.html',
   styleUrl: './performance-levels.component.scss'
 })
