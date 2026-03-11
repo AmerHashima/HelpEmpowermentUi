d06ee5c0873f4f8b78dde06900b7c911a08d41db
Amer Hashima <ameer.hashima@hispeed-erp.com>
2026-03-11 20:41:07 +0200

    refactor: improve code formatting and consistency in student service and environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://localhost:44379/api',
+  baseUrl: 'http://144.91.127.150:8080/api',
   mailServiceId: "service_qa9ozck",
   mailTemolateId: "template_1sqbwoa",
   mailPublicKey: "o3-i7ksFLJZT2C6GV",
