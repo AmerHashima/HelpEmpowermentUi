a6846df189ba28e300aca01427fac1b718beece9
Amer Hashima <ameer.hashima@hispeed-erp.com>
2026-07-14 08:02:51 +0300

    fix: Update production environment settings and base URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://144.91.127.150:8080/api',
+  baseUrl: 'http://144.91.127.150:8081/api',
+  telrAllowedHosts: ['secure.telr.com'],
   mailTemolateId: "template_dhwmgqo",
   mailServiceId: "service_lc3tdtq",
   resetTemolateId: "template_awvrr8h",
